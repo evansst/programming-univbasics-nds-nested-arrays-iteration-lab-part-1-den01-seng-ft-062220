577166606adeee3d0dcc7f7d95b4e1cd6a17dffa
Sam Evans <evansst@icloud.com>
2020-06-16 02:35:32 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,5 +7,12 @@ def find_even_values(src)
     e_index = 0 
     while e_index < src[r_index].count do
       if src[r_index][e_index].even?
+        p src[r_index][e_index]
+      end
+      e_index += 1 
+    end
+    r_index += 1 
+  end
+  
 
 end
